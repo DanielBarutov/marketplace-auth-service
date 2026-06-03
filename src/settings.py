cfbf3d5a7542e5afe7f893baafdcc528e0fb3daa
Daniel Barutov <barutovdg@gmail.com>
2026-06-03 12:00:24 +0300

    added annotation

--- src/settings.py
+++ src/settings.py
@@ -5,7 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-    still_url = os.getenv("POSTGRES_CONNECTION_STRING")
+    still_url: str | None = os.getenv("POSTGRES_CONNECTION_STRING")
     if still_url:
         database_url: str = still_url.replace("postgres:", "postgresql+asyncpg:", 1)
     else:
